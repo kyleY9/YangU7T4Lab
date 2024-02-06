81b9486095a06bc49e90834fea5d7b978cd89109
KyleY9 <KyleY9@nycstudents.net>
2024-02-06 12:29:02 -0500

    Made some progress

---
+++ src/ArrayListAlgorithms.java
@@ -0,0 +1,83 @@
+import java.util.ArrayList;
+
+public class ArrayListAlgorithms {
+    /**
+     * Returns true if any of the elements in stringList contain
+     * target as a substring; false otherwise.  Use indexOf instead of contains
+     * for AP practice! (although in reality, contains is preferred)
+     * <p>
+     * Does NOT mutate (modify) elements of stringList.
+     * PRECONDITION: stringList.size() > 0
+     *
+     * @param stringList original arraylist of Strings
+     * @return true if target is found in any of the strings, false otherwise
+     */
+    public static boolean containsTarget(ArrayList<String> stringList, String target) {
+        for (String str : stringList) {
+            if (str.indexOf(target) != -1) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /** Returns number of elements in intList that are less than the
+     *  average of all elements.
+     *
+     *  Does NOT mutate (modify) elements of intList.
+     *  PRECONDITION: intList.size() > 0
+     *
+     *  @param intList  original arraylist of Integers
+     *  @return  the number of ints in intList that are less than the average
+     */
+    public static int belowAverage(ArrayList<Integer> intList) {
+        double avg = 0.0;
+        for (int num : intList) {
+            avg += num;
+        }
+        avg /= intList.size();
+
+        int count = 0;
+        for (int num : intList) {
+            if (num < avg) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    /** Replaces all words in wordList that end in "s" with the all-uppercase
+     *  version of the word.  For example, "apples" should be replaced with "APPLES".
+     *  Assume all letters of all words in wordList are lowercase initially (no need
+     *  to worry about checking for case or converting first to lowercase)
+     *
+     *  DOES mutate (modify) elements of wordList.
+     *  PRECONDITION: wordList.size() > 0
+     *
+     *  @param wordList  original arraylist of words
+     */
+    public static void replaceWithCaps(ArrayList<String> wordList) {
+        for (int i = 0; i < wordList.size(); i++) {
+            if (wordList.get(i).charAt(wordList.get(i).length() - 1) == 's') {
+                wordList.set(i, wordList.get(i).toUpperCase());
+            }
+        }
+    }
+
+    /** Returns the index at which the minimum value of all integers in
+     *  intList appears; if the minimum value appears more than once in
+     *  the arraylist, return the index of the first occurrence
+     *
+     *  Does NOT mutate (modify) elements in intList.
+     *  PRECONDITION: intList.size() > 0
+     *
+     *  @param intList  original arraylist of Integers
+     *  @return  the index at which the minimum value occurs
+     */
+    public static int indexOfMinimum(ArrayList<Integer> intList) {
+        int minIdx = Integer.MAX_VALUE;
+        for (int i = 0; i < intList.size(); i++) {
+
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,42 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // method #1 - containsTarget
+        ArrayList<String> stringList = new ArrayList<String>(Arrays.asList("this", "list", "contains", "several", "words"));
+        boolean contains = ArrayListAlgorithms.containsTarget(stringList, "is");
+        System.out.println(contains);
+        contains = ArrayListAlgorithms.containsTarget(stringList, "v");
+        System.out.println(contains);
+        contains = ArrayListAlgorithms.containsTarget(stringList, "words");
+        System.out.println(contains);
+        contains = ArrayListAlgorithms.containsTarget(stringList, "ia");
+        System.out.println(contains);
+        System.out.println();
+
+        // method #2 belowAverage
+        ArrayList<Integer> intList = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5));
+        int count = ArrayListAlgorithms.belowAverage(intList);
+        System.out.println(count);
+        ArrayList<Integer> intList2 = new ArrayList<Integer>(Arrays.asList(6, 3, 8, 5, 6, 3));
+        count = ArrayListAlgorithms.belowAverage(intList2);
+        System.out.println(count);
+        ArrayList<Integer> intList3 = new ArrayList<Integer>(Arrays.asList(10, 12, 9));
+        count = ArrayListAlgorithms.belowAverage(intList3);
+        System.out.println(count);
+        System.out.println();
+
+        // method #3 - replaceWithCaps
+        ArrayList<String> stringList2 = new ArrayList<String>(Arrays.asList("this", "list", "contains", "several", "words"));
+        ArrayListAlgorithms.replaceWithCaps(stringList2);
+        System.out.println(stringList2);
+
+        ArrayList<String> stringList12 = new ArrayList<String>(Arrays.asList("yes", "pass", "buts", "Mets", "Yankees"));
+        ArrayListAlgorithms.replaceWithCaps(stringList12);
+        System.out.println(stringList12);
+
+        ArrayList<String> stringList13 = new ArrayList<String>(Arrays.asList("yeah", "pat", "but", "Met", "Yankee"));
+        ArrayListAlgorithms.replaceWithCaps(stringList13);
+        System.out.println(stringList13);
+        System.out.println();
+    }
+}
